b61d574223e79cb358ddd4f36f8110f5e5d314b3
bear <bear@yikes.com>
2011-07-05 08:59:46 -0700

    memory recording support.

--- answer.py
+++ answer.py
@@ -5,12 +5,14 @@ import os
 
 import scipy
 import scipy.weave
+import numpy
 
 # Change these values such that they match your installation of pjsua. You
 # probably will need to recompile PJSUA with -fPIC, and you will probably
 # want to grab the correct values form a working C Makefile
 support_code = """ 
 #include <pjlib.h>
+#include <pjmedia.h>
 """
 libraries="""pjsua-x86_64-unknown-linux-gnu pjsip-ua-x86_64-unknown-linux-gnu pjsip-simple-x86_64-unknown-linux-gnu pjsip-x86_64-unknown-linux-gnu pjmedia-codec-x86_64-unknown-linux-gnu pjmedia-x86_64-unknown-linux-gnu pjmedia-audiodev-x86_64-unknown-linux-gnu pjnath-x86_64-unknown-linux-gnu pjlib-util-x86_64-unknown-linux-gnu resample-x86_64-unknown-linux-gnu milenage-x86_64-unknown-linux-gnu srtp-x86_64-unknown-linux-gnu gsmcodec-x86_64-unknown-linux-gnu speex-x86_64-unknown-linux-gnu ilbccodec-x86_64-unknown-linux-gnu g7221codec-x86_64-unknown-linux-gnu portaudio-x86_64-unknown-linux-gnu  pj-x86_64-unknown-linux-gnu m uuid nsl rt pthread  asound crypto ssl""".split()
 library_dirs="""/home/bear/pjproject-1.10/pjlib/lib /home/bear/pjproject-1.10/pjlib-util/lib /home/bear/pjproject-1.10/pjnath/lib /home/bear/pjproject-1.10/pjmedia/lib /home/bear/pjproject-1.10/pjsip/lib /home/bear/pjproject-1.10/third_party/lib""".split()
@@ -29,16 +31,13 @@ class answerPhoneCB(pjsua.AccountCallback):
 
   # Notification on incoming call
   def on_incoming_call(self, call):
-    print "Got a connection from: ", call.info().remote_uri
+    print "Got a connection from: ", call.info().remote_uri, call
 
     call_cb = AnswerLoopCB( call )
     call.set_callback(call_cb)
-
-    call.answer(180) # Set intent to answer.
-    code=""" pj_thread_sleep(1000); """
-    scipy.weave.inline(code, support_code=support_code, libraries=libraries,
-                       library_dirs=library_dirs, extra_link_args=["-fPIC"])
     call.answer(200) # Answer call
+
+
 
 
     
@@ -73,6 +72,33 @@ class AnswerLoopCB(pjsua.CallCallback):
     self.pj = pj
     self.counter=0
     call_slot = self.call.info().conf_slot
+
+    """ Initialize Memory based recorder """
+
+    conversation = numpy.zeros ( 14745600, dtype=numpy.uint16 ) # 15m @ 16khz
+
+    mcode = """
+pj_caching_pool cp;
+pjmedia_endpt *med_endpt;
+pj_pool_t *pool;
+pjmedia_port *mem_port;
+
+pj_caching_pool_init(&cp, &pj_pool_factory_default_policy, 0);
+if ( pjmedia_endpt_create(&cp.factory, NULL, 1, &med_endpt) != PJ_SUCCESS )
+  return -1; 
+
+pool = pj_pool_create( &cp.factory, "mrec", 4000, 4000, NULL  );
+port = pjmedia_mem_capture_create  (   pool, conversation, 14745600*2, 8000, 1, 80, 16, 0, &mem_port );
+
+return Py::new_reference_to(Py::Int(port))
+"""
+
+    try: 
+      scipy.weave.inline(mcode, ['conversation'], support_code=support_code, 
+                       libraries=libraries, library_dirs=library_dirs, 
+                       extra_link_args=["-fPIC"] ) 
+    except Exception, e:
+      print "pjmedia_mem_capture_create error", e
 
     if self.call.info().media_state == pjsua.MediaState.ACTIVE:
       self.counter+=1
@@ -132,6 +158,7 @@ mediaConf = pjsua.MediaConfig()
 #mediaConf.clock_rate = 22050
 #mediaConf.channel_count=1
 #mediaConf.snd_auto_close_time=900
+mediaConf.enable_ice=1
 
 try:
   code=""" pj_thread_sleep(1000); """
